081c337867329bb1bc1419f3d3369e1fd7fc1958
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-02-02 19:31:45 +0300

    sddsfds :wq

---
+++ zad_19/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int pol = number.Length;
+Console.WriteLine(Polindrom(pol));
+
+string Polindrom (int pol)
+{
+    string res;
+if (pol == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3]) res = "Число полиндром";
+    else res = "Число не полиндром";
+}
+else res = "Введите именно пятизначное!!!";
+return res;
+}
